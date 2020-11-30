0618f86ad1588eecdbc0db105a56db34807e7cbb
Elikem Dablor <elikemdablor@gmail.com>
2020-11-30 12:04:23 +0000

    Skeleton 1-API

---
+++ API/Data/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO `Users` (Id, UserName)
+VALUES (1, "Kwame");
+
+INSERT INTO `Users` (Id, UserName)
+VALUES (2, "Adjo");
+
+INSERT INTO `Users` (Id, UserName)
+VALUES (3, "Yaw");
